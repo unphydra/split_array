c4c1822ed304893dfdc13cf1c0c313bc1b0fb0d9
rivu <58026024+unphydra@users.noreply.github.com>
2020-04-22 16:56:33 +0530

    modified get_split_array_by_range function that will now only return an empty final but print three range

--- splitArray.c
+++ splitArray.c
@@ -6,6 +6,30 @@
 Split_Array *get_split_array_by_range(Array num,Array range,int length)
 {
   Split_Array * final;
+  int first[length];
+  int count_f = 0;
+  int second[length];
+  int count_s = 0;
+  int third[length];
+  int count_t = 0;
+  for (int i = 0; i < length; i++)
+  {
+    if(num[i]<range[0]){
+      first[count_f] = num[i];
+      count_f++;
+      continue;
+    }
+    if(num[i]>range[1]){
+      third[count_t] = num[i];
+      count_t++;
+      continue;
+    }
+    second[count_s] = num[i];
+    count_s++;
+  }
+  printf("%d %d %d\n",first[0],first[1],first[2]);
+  printf("%d %d %d %d\n",second[0],second[1],second[2],second[3]);
+  printf("%d",third[0]);
   return final;
 }
 
